2dda0ab60484f1a6a3cfe28ea01e36fe6cf3d7de
Barbara LEFEBVRE <blefebvr@f4r1s14.clusters.42paris.fr>
2025-07-25 16:23:28 +0200

    add ex00-01 php starting

--- Symfony_0_Starting/ex00/var.php
+++ Symfony_0_Starting/ex00/var.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+$a = 10;
+$b = "10";
+$c = "ten";
+$d = 10.0;
 
-
+echo "My first variables:\n";
+echo "a contains : 10 and has type : " . gettype($a);
+echo "\nb contains : 10 and has type : " . gettype($b);
+echo "\nc contains : ten and has type : " . gettype($c);
+echo "\nd contains : 10 and has type : " . gettype($d);
 ?>
--- Symfony_0_Starting/ex01/csv.php
+++ Symfony_0_Starting/ex01/csv.php
@@ -1,4 +1,14 @@
 <?php 
 
+$s = file_get_contents('ex01.txt');
+$s1 = str_split($s);
+$s2 = array_map('trim', $s1);
+$i = 0;
+while ($i < count($s2)) {
+    echo $s2[$i];
+    $i++;
+}
+// fputcsv($f, ",");
+// fclose($f);
 
 ?>
