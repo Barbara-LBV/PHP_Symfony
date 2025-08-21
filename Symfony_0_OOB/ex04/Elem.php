309144535ceb193adcae83687c5a8f50c6192ac8
root <root@LAPTOP-GKSLV4TL>
2025-08-21 19:47:04 +0200

    ex04 wip

--- Symfony_0_OOB/ex04/Elem.php
+++ Symfony_0_OOB/ex04/Elem.php
@@ -1,94 +1,176 @@
 <?php 
 
+include './MyException.php';
+
 class Elem {
 
     public string $content;
     public string $element;
+    public array $attributes;
     public array $htmlElements = []; // array of strings
-    public string $result = '';
+    public array $autoClosing = ['meta', 'br', 'hr', 'img'];
     public array $priorityTags = ['html', 'head', 'meta', 'title', 'body'];
-    public array $tags = [
-        'html', 'head', 'meta', 'title', 'body', 'img', 'hr', 'br', 
-        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'div'
-    ];
-
-    public function __construct(string $element, string $content = '') {
-        if (!in_array($element, $this->tags)) {
-            print("Error: Input is not an supported HTML tag.\n");
-            return false;
+    public array $tags = ['html', 'head', 'meta', 'title', 'body', 'div','p', 'img',
+        'hr', 'br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span'];
+    
+    public function __construct(string $element, string $content = '', array $attributes = []) {
+        $exc = new MyException();
+        try {
+            if (!is_string($element) || !is_string($content) || !is_array($attributes)) 
+                throw new MyException();
+            
+            if (!in_array($element, $this->tags)) 
+                throw new MyException();
+            
+            $this->element = $element;
+            $this->content = $content;
+            $this->attributes = $attributes;
+            $key = array_search($this->element, $this->tags);
+            if (!empty($attributes))
+                $value = "<{$this->element} {$attributes[key]}={$attributes[0]}>";
+            switch ($key){
+                case 0: // html tag
+                    $this->htmlElements[$key] = "<{$this->element}>";
+                    break;
+                case 1: // head tag
+                    $this->htmlElements[$key] = "<{$this->element}>";
+                    break;
+                case 2: // meta tag
+                    $this->htmlElements[$key] = $value . "/>";
+                    break;
+                case 3: // title tag
+                    $this->htmlElements[$key] = "<{$this->element}>{$this->content}</{$this->element}>";
+                    break;
+                case 4: // body tag
+                    $this->htmlElements[$key] = "<{$this->element}>";
+                    break;
+                case 5: // div tag
+                    if ($this->content !== '')
+                        $this->htmlElements[$key] = "<{$this->element}>{$this->content}</{$this->element}>";
+                    else
+                        $this->htmlElements[$key] = "<{$this->element}>";
+                    break;
+                case 6: // p tag
+                    if ($this->content !== '')
+                        $this->htmlElements[$key] = "<{$this->element}>{$this->content}</{$this->element}>";
+                    else
+                        $this->htmlElements[$key] = "<{$this->element}>";
+                    break;
+                default: // other tags
+                    if (in_array($this->element, $this->autoClosing)) 
+                        $this->htmlElements[] = "<{$this->element} {$this->content}/>";
+                    elseif(in_array($this->element, $this->tags)) 
+                        $this->htmlElements[] = "<{$this->element}>{$this->content}</{$this->element}>";
+                    break;
+                }
+                ksort($this->htmlElements);
+        } catch (Exception $e) {
+            echo $e->getMessage() . "\n";
         }
-        $this->element = $element;
-        $this->content = $content;
+        // $this->element = $element;
+        // $this->content = $content;
+        // $this->attributes = $attributes;
+        // $key = array_search($this->element, $this->tags);
 
-        $autoClosing = ['meta', 'br', 'hr', 'img'];
-        $key = array_search($this->element, $this->priorityTags);
-        if (in_array($this->element, $this->priorityTags) && in_array($this->element, $autoClosing)) {
-            $this->htmlElements[$key] = "<{$this->element} {$this->content}/>";
-        } elseif (in_array($this->element, $this->priorityTags)) {
-            $this->htmlElements[$key] = "<{$this->element}>{$this->content}";
-        } else if (in_array($this->element, $autoClosing)) {
-            $this->htmlElements[5] = "<{$this->element} {$this->content}/>";
-        } else
-            $this->htmlElements[5] = "<{$this->element}>{$this->content}</{$this->element}>";
-        ksort($this->htmlElements);
+        // switch ($key){
+        //     case 0: // html tag
+        //         $this->htmlElements[$key] = "<{$this->element}>";
+        //         break;
+        //     case 1: // head tag
+        //         $this->htmlElements[$key] = "<{$this->element}>";
+        //         break;
+        //     case 2: // meta tag
+        //         $this->htmlElements[$key] = "<{$this->element} {$this->content}/>";
+        //         break;
+        //     case 3: // title tag
+        //         $this->htmlElements[$key] = "<{$this->element}>{$this->content}</{$this->element}>";
+        //         break;
+        //     case 4: // body tag
+        //         $this->htmlElements[$key] = "<{$this->element}>";
+        //         break;
+        //     case 5: // div tag
+        //         if ($this->content !== '')
+        //             $this->htmlElements[$key] = "<{$this->element}>{$this->content}</{$this->element}>";
+        //         else
+        //             $this->htmlElements[$key] = "<{$this->element}>";
+        //         break;
+        //     case 6: // p tag
+        //         if ($this->content !== '')
+        //             $this->htmlElements[$key] = "<{$this->element}>{$this->content}</{$this->element}>";
+        //         else
+        //             $this->htmlElements[$key] = "<{$this->element}>";
+        //         break;
+        //     default: // other tags
+        //         if (in_array($this->element, $this->autoClosing)) 
+        //             $this->htmlElements[] = "<{$this->element} {$this->content}/>";
+        //         elseif(in_array($this->element, $this->tags)) 
+        //             $this->htmlElements[] = "<{$this->element}>{$this->content}</{$this->element}>";
+        //         break;
+        //     }
+        //     ksort($this->htmlElements);
         }
 
     public function pushElement(Elem $elem): void {
         if (!$elem instanceof Elem || !$elem) {
             print("Error: Invalid parameter: must be an Elem object.\n");
-            return;
+            return ;
         }
         if ($elem->htmlElements) {
             foreach ($elem->htmlElements as $key => $value) {
                 if (!array_key_exists($key, $this->htmlElements))
                     $this->htmlElements[$key] = $value;
-                elseif ($this->htmlElements[$key] && $key > 4)
+                elseif ($value == ('<div>' || '<p>') && in_array($value, $this->htmlElements)) {
+                    $this->htmlElements[] = $value;
+                } elseif (in_array($value, $this->htmlElements))
+                    continue ;
+                elseif (!in_array($value, $this->priorityTags))
                     $this->htmlElements[] = $value;
             }
         } else {
             print("Error: Element has no HTML content.\n");
-            return;
+            return ;
         }
         ksort($this->htmlElements);
     }
 
     public function getHTML(): string {
+        $this->result = '';
+        $openTags = [];
+        // reset indexes of htmlElements to ensure no duplicates
         if (empty($this->htmlElements)) {
             print("Error: No HTML elements to render.\n");
             return '';
         }
-
-        // adding closing tags for priority tags
-        for ($i = 4 ; $i > -1 ; $i--) {
-            $key = array_key_exists($i, $this->htmlElements);
-            if ($key === true && $i != 2)  {
-                $value = "</{$this->priorityTags[$i]}>";
-                if (!in_array($value, $this->htmlElements))
-                    $this->htmlElements[] = $value;
+        $this->htmlElements = array_values($this->htmlElements);
+        $maxIndex = count($this->htmlElements);
+        
+        for ($i = 0; $i < $maxIndex ; $i++) {
+            // check if the index exists in htmlElements
+            if (array_key_exists($i, $this->htmlElements)) {
+                $element = $this->htmlElements[$i];
+                // if opening tag, add it to openTags
+                if (preg_match('/<([a-zA-Z0-9]+)(?:\s[^>]*)?>$/', $element, $matches)) {
+                    $tagName = $matches[1];
+                    if (!in_array($tagName, $this->autoClosing)) {
+                        array_push($openTags, $tagName);
+                    }
+                }
+                $this->result .= $this->indentElement($element, count($openTags) - 1);
             }
         }
-        
-        $this->result = formatingHTML($this->htmlElements);
+        // closing remaining open tags
+        while (!empty($openTags)) {
+            $tagToClose = array_pop($openTags);
+            $this->result .= str_repeat('  ', count($openTags)) . "</{$tagToClose}>\n";
+        }
         return $this->result;
     }
-}
-
-function formatingHTML(array $a): string {
-    $indent = 0;
-    $result = "";
-
-    foreach ($a as $tag) {
-        if (preg_match('/^<\//', $tag)) {
-            $indent -= 2; //closing tag, decreasing before
-        }
-        if ($indent < 0)
-            $indent = 0; // avoid negative indentation
-        $result .= str_repeat("  ", $indent) . $tag . "\n";
-        if (preg_match('/^<(?!\/)(?!.*\/>)/', $tag)) {
-            $indent++; // opening tag, increasing after
-        }
+    
+    private function indentElement(string $element, int $level): string {
+        $indent = str_repeat('  ', $level);
+        return $indent . $element . "\n";
     }
-    return $result;
+
 }
 
 ?>
--- Symfony_0_OOB/ex04/MyException.php
+++ Symfony_0_OOB/ex04/MyException.php
@@ -1,4 +1,10 @@
 <?php 
 
+class MyException extends Exception {
+
+    public function __construct() {
+        parent::__construct("MyException occurred");
+    }
+}
 
 ?>
