6899e6bb914c36f6a245258af82aab5b02279be8
Barbara LEFEBVRE <blefebvr@f4r13s12.clusters.42paris.fr>
2025-09-08 18:12:08 +0200

    last check php starting wip

--- Symfony_0_Starting/ex01/csv.php
+++ Symfony_0_Starting/ex01/csv.php
@@ -3,17 +3,16 @@
 $filename = "ex01.txt";
 
 if (file_exists($filename)) {
-    $contenu = file_get_contents($filename);
-    $valeurs = explode(',', $contenu);
+    $content = file_get_contents($filename);
+    $valeurs = explode(',', $content);
     foreach ($valeurs as $valeur) {
-        echo trim($valeur) . "\n";
+        print(trim($valeur) . "\n");
     }
 } else {
     echo "No such file or directory: $filename\n";
 }
+/* file_get_contents() reads the entire file into a string
+explode() splits the string into an array using the specified delimiter (comma in this case)
+trim() removes whitespace from the beginning and end of a string
+foreach() iterates over each element in the array and prints it*/
 ?>
-
-//file_get_contents() reads the entire file into a string
-//explode() splits the string into an array using the specified delimiter (comma in this case)
-//trim() removes whitespace from the beginning and end of a string
-//foreach() iterates over each element in the array and prints it
